3be2f076eaeaa58ad48b8ac33cc8a0a3460216e0
ryomaruu <120063290+ryomaruu@users.noreply.github.com>
2022-12-15 14:20:37 +0900

    htmlで画像アップロードできるようになった

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 from flask import Flask, request, render_template, jsonify
-import json  # Python標準のJSONライブラリを読み込んで、データの保存等に使用する
-
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False  # 日本語などのASCII以外の文字列を返したい場合は、こちらを設定しておく
 
-# http://127.0.0.1:5000/address
-@app.route('/address', methods=["GET"])
-def image_upload():
-    pass
 
 # http://127.0.0.1:5000/
 @app.route('/')
