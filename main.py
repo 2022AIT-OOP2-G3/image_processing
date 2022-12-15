936cb41ef8618e9241b82586bd9453f7d1e54015
ryomaruu <120063290+ryomaruu@users.noreply.github.com>
2022-12-15 13:57:49 +0900

    main.pyの型作成

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template, jsonify
+import json  # Python標準のJSONライブラリを読み込んで、データの保存等に使用する
+
+app = Flask(__name__)
+app.config["JSON_AS_ASCII"] = False  # 日本語などのASCII以外の文字列を返したい場合は、こちらを設定しておく
+
+# http://127.0.0.1:5000/address
+@app.route('/address', methods=["GET"])
+def image_upload():
+    pass
+
+# http://127.0.0.1:5000/
+@app.route('/')
+def index():
+    return render_template("upload_page.html")
+
+if __name__ == "__main__":
+    # debugモードが不要の場合は、debug=Trueを消してください
+    app.run(debug=True)
