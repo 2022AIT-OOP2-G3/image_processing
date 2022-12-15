03c903dc734d84085e6926d29821ccc1c746280e
Bayashin <k21099kk@aitech.ac.jp>
2022-12-15 15:10:34 +0900

    update:pythonによるhtmlの書き換え

--- module/upload_list.py
+++ module/upload_list.py
@@ -1,7 +1,7 @@
 import glob
 
-def add_image() -> None:
-    dir = '../uplead_imag/*.jpg'
+def add_image() -> str:
+    dir = '../upload_imag/*.jpg'
     count_file = 0
     index = []
     files = glob.glob(dir)
@@ -9,4 +9,20 @@ def add_image() -> None:
         count_file+=1
         index.append(file)
 
-    
+    page_data = {}
+    if count_file == 0:
+        page_data['image'] = '画像は投稿されていません'
+    else:
+        page_data['image'] = '<li><img src = '+ index(0) +' width="350" height="200" alt="画像1"></li>'
+        i=1
+        for i in count_file:
+            page_data['image'] = page_data['image'] + '<li><img src = '+ index(i) +' width="350" height="200" alt="画像'+(i+1)+'"></li>'
+
+    with open('../templates/upload_html','r') as f:
+        html = f.read()
+    f.closed
+
+    for key, value in page_data.items():
+        html = html.replace('{%' + key + '%}'), value
+
+    return html
