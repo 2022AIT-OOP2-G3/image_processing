9b3067238242437587c9956928d236614884e06f
ryomaruu <120063290+ryomaruu@users.noreply.github.com>
2022-12-15 15:19:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, jsonify, redirect, url_for
+import os
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False  # 日本語などのASCII以外の文字列を返したい場合は、こちらを設定しておく
 
+# http://127.0.0.1:5000/
+@app.route('/upload',methods=['POST','GET'])
+def upload_kanryou():
+    # if request.method=='GET':
+    #     return render_template('upload_page.html')
+    if request.method=='POST':
+        # file=request.files=['test']
+        # file.save(os.path.join('./static'),'a')
+        return render_template("upload_page.html",message="アップロード完了")
 
 # http://127.0.0.1:5000/
 @app.route('/')
